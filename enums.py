14b0f4b6cda7b87dcb24fa35318f454341043b95
DamianWorobiej <Damian.Worobiej@gmail.com>
2025-11-11 15:24:02 +0100

    Add handlers for onboard led for some raspberry boards

---
+++ enums.py
@@ -0,0 +1,10 @@
+class Board:
+    Pico = 1
+    PicoZero = 2
+
+class RGB:
+    Off = (0, 0, 0)
+    White = (255, 255, 255)
+    Red = (0, 255, 0)
+    Green = (255, 0, 0)
+    Blue = (0, 0, 255)
---
+++ onboard_led.py
@@ -0,0 +1,46 @@
+from machine import Pin
+from neopixel import NeoPixel
+from enums import Board, RGB
+
+class OnboardLed():
+    def __init__(self, board: int = Board.Pico) -> None:
+        self.led: LedBase = LedBase()
+        if board is Board.Pico:
+            self.led = PicoLed()
+        if board is Board.PicoZero:
+            self.led = PicoZeroLed()
+
+    def on(self):
+        self.led.on()
+
+    def off(self):
+        self.led.off()
+
+class LedBase():
+    def on(self):
+        pass
+
+    def off(self):
+        pass
+
+class PicoZeroLed(LedBase):
+    def __init__(self) -> None:
+        self.led = NeoPixel(Pin(16), 1)
+
+    def on(self):
+        self.led[0] = RGB.White
+        self.led.write()
+
+    def off(self):
+        self.led[0] = RGB.Off
+        self.led.write()
+
+class PicoLed(LedBase):
+    def __init__(self) -> None:
+        self.led = Pin(16, Pin.OUT)
+
+    def on(self):
+        self.led.value(1)
+
+    def off(self):
+        self.led.value(0)
