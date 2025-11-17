75228d2c31f0d2a5cbfbe1d83fb206f51c93ea4b
DamianWorobiej <Damian.Worobiej@gmail.com>
2025-11-17 11:34:56 +0100

    Add battery types to enums

--- enums.py
+++ enums.py
@@ -15,3 +15,25 @@ class GRB:
     Red = (0, 255, 0)
     Green = (255, 0, 0)
     Blue = (0, 0, 255)
+
+class BaseBattery:
+    def __init__(self) -> None:
+        self.__max_voltage = 0
+        self.__min_voltage = 0
+        self.__rated_voltage = 0
+
+    def get_max_voltage(self) -> float:
+        return self.__max_voltage
+    
+    def get_min_voltage(self) -> float:
+        return self.__min_voltage
+    
+    def get_rated_voltage(self) -> float:
+        return self.__rated_voltage
+
+class Battery3p7V(BaseBattery):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__max_voltage = 4.2
+        self.__min_voltage = 2.5
+        self.__rated_voltage = 3.7
