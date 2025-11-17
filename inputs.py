8f3c68bd1bfbf7de1f1862dc9aab55b0cc9cea2c
DamianWorobiej <Damian.Worobiej@gmail.com>
2025-11-17 11:36:46 +0100

    Include battery level control in inputs

--- inputs.py
+++ inputs.py
@@ -1,5 +1,9 @@
-from machine import Pin
+from machine import Pin, ADC
 
 class Inputs:
-    def __init__(self, sensor_pin_number):
-      self.sensor = Pin(sensor_pin_number, Pin.IN)
+    def __init__(self, sensor_pin_number: int, battery_level_controller_pin_number: int | None = None):
+      self.sensor: Pin = Pin(sensor_pin_number, Pin.IN)
+      self.battery_control_enabled = False
+      if battery_level_controller_pin_number is not None:
+        self.battery_control_enabled = True
+        self.battery_voltage_signal: ADC = ADC(Pin(battery_level_controller_pin_number))
